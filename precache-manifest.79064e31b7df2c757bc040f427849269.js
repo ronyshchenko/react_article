3a4badd639ab776e8da71704d8ad7dfba0d526ad
ronyshchenko <roma.onyshchenko@gmail.com>
2020-02-17 13:52:37 +0200

    Updates

---
+++ precache-manifest.79064e31b7df2c757bc040f427849269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7354cf41691d437f290dafee76bd8db6",
+    "url": "/github.io/react_article/index.html"
+  },
+  {
+    "revision": "91558b3dfd64ea894eea",
+    "url": "/github.io/react_article/static/js/2.433df800.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/github.io/react_article/static/js/2.433df800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab36a8f67e3e5316e10",
+    "url": "/github.io/react_article/static/js/main.89496ddd.chunk.js"
+  },
+  {
+    "revision": "283a61b963dc37d65fa0",
+    "url": "/github.io/react_article/static/js/runtime-main.eb713a77.js"
+  }
+]);
